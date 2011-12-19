8ecef852df0d920b4b6633131057a3e7fb48c033
Gun.io Whitespace Robot <contact@gun.io>
2011-12-19 07:35:38 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- src/NState/IStateTransition.cs
+++ src/NState/IStateTransition.cs
@@ -5,7 +5,7 @@
     /// <summary>
     ///   Defines the interface for a state transition for a type that supports
     ///   "workflow". Examples of actions being performed during a transition
-    ///   include the sending of an email or SMS, the persisting of information 
+    ///   include the sending of an email or SMS, the persisting of information
     ///   to the database, or an onscreen notification or the billing of funds.
     ///   Note that a computer program might invoke the state transition.
     /// </summary>
--- src/clean.bat
+++ src/clean.bat
@@ -1,4 +1,4 @@
-cd /d %0\.. 
+cd /d %0\..
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S _ReSharper.*') DO RMDIR /S /Q "%%G"
