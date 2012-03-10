d0047cf4c47fab9a9ad48a3afad1c63fb371fe00
Ben Aston <ben.aston@justgiving.com>
2012-03-10 14:43:47 +0000

    Guid change attempt to fix nuget name.

--- src/NState/Properties/AssemblyInfo.cs
+++ src/NState/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("09b7aff0-16ef-480c-ab07-e4397d2b5912")]
+[assembly: Guid("09b7aff0-16ef-480c-ab07-e4397aab5912")]
 [assembly: AssemblyVersion("1.0.*")]
