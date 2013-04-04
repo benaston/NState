1e8bcdf739f4485046d750bdce9acd7feb8f5892
Ben Aston <ben.aston@rbi.co.uk>
2013-04-04 10:50:37 +0100

    Updated description and copyright. <Ben>

--- src/NState/Properties/AssemblyInfo.cs
+++ src/NState/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NState")]
-[assembly: AssemblyDescription("A simple state machine implementation.")]
+[assembly: AssemblyDescription("A simple state machine implementation. If you like NState, please add an entry to the users.md file in the GitHub repo and open a pull request. Let's share descriptions of the cool things we are using NState for.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ben Aston")]
 [assembly: AssemblyProduct("NState")]
